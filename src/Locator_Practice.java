3226250154a57b333cad6f6a8765f30385a0f2ab
[DaninTexas] <[daniel.gail@gmail.com]>
2018-10-13 22:39:41 -0500

    Finishing training for the day

--- src/Locator_Practice.java
+++ src/Locator_Practice.java
@@ -1,5 +1,14 @@
 // Locator learning and practice
 // Over commented for learning sake
+// Ways to locate
+// ID - Class - CSS Selector - XPATH
+
+
+/*
+ * You can validate XPATH and CSS selector in the 'Console' of the browser. 
+ * XPATH = $x("//*[@id='contactForm']/div[6]/div/button")
+ * CSS Selector = $("#message")
+ */
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -18,14 +27,22 @@ public class Locator_Practice
 		
 		// Find elements and fill out the form
 		driver.findElement(By.id("name")).sendKeys("Daniel Gail");
-		// driver.findElement(By.id("email")).sendKeys("daniel.gail@gmail.com");
-		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("daniel.gail@gmail.com");;
-		driver.findElement(By.id("phone")).sendKeys("832-612-1753");
-		driver.findElement(By.id("message")).sendKeys("Yeah this works! I am loving learning this stuff! Just too much fun!");
 		
-		// Click on the Send key for the form
+		// driver.findElement(By.id("email")).sendKeys("daniel.gail@gmail.com");
+		// Use xpath - CHange double quotes to singles
+		driver.findElement(By.xpath("//*[@id=\'email\']")).sendKeys("daniel.gail@gmail.com");
+		
+		driver.findElement(By.id("phone")).sendKeys("832-612-1753");
+		
+		//driver.findElement(By.id("message")).sendKeys("Yeah this works! I am loving learning this stuff! Just too much fun!");
+		// Using CSS Selector
+		driver.findElement(By.cssSelector("#message")).sendKeys("Yeah this works! I am loving learning this stuff! Just too much fun!");
+		
+		// Click on the 'Send' key for the form
 		// Can not use multiple classes with this
-		driver.findElement(By.className("btn")).click();
+		// driver.findElement(By.className("btn")).click();
+		// Using XPATH
+		driver.findElement(By.xpath("//*[@id=\'contactForm\']/div[6]/div/button")).click();
 		
 		// Shutdown chromedriver
 		driver.quit();
